4f3973ac2f5ec7aaf9b1cbd53b68d8909de18a00
Brett O'Donnell <cornernote@gmail.com>
2015-02-10 09:01:26 +1030

    Create Application.php

---
+++ mrphp/components/Application.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Skeleton Application
+ *
+ * @method static Application createInstance($config = array(), $id = null)
+ * @method static Application instance($id = null)
+ */
+class Application extends MrInstance
+{
+
+    /**
+     *
+     */
+    public function process()
+    {
+        echo 'Application Running...';
+    }
+    
+} 
